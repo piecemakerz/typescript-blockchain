469ef2b6bd40da3c93d6aedb881f878377eb3956
piecemakerz <piecemakerz@naver.com>
2020-10-08 01:10:57 +0900

    feat: Learn Interface in Typescript

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,18 @@
-const sayHi = (name: string, age: number, gender?: string): void => {
+interface Human {
+  name: string;
+  age: number;
+  gender: string;
+}
+
+const person = {
+  name: "piecemakerz",
+  gender: "male",
+  age: 24,
+};
+
+const sayHi = ({ name, age, gender }: Human): void => {
   console.log(`Hi, I'm ${name} and age of ${age}, whose gender is ${gender}`);
 };
 
-sayHi("piecemakerz", 24, "male");
+sayHi(person);
 export {};
