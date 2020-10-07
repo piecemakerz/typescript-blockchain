5da4f94eb2109cde66e9efd93b32949465fb2373
piecemakerz <piecemakerz@naver.com>
2020-10-08 00:53:47 +0900

    feat: 타입스크립트 환경 설정 및 테스트 함수 작성

---
+++ index.ts
@@ -0,0 +1,10 @@
+const name = "piecemakerz",
+  age = 24,
+  gender = "male";
+
+const sayHi = (name, age, gender?) => {
+  console.log(`Hi, I'm ${name} and age of ${age}, whose gender is ${gender}`);
+};
+
+sayHi(name, age);
+export {};
