c922ea8290c10f2886e5f8945cd09975ad40a6cb
piecemakerz <piecemakerz@naver.com>
2020-10-08 01:29:18 +0900

    feat: Apply cryptoJS and Implement real-life blockchain instances

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
+import * as CryptoJS from "crypto-js";
+
 class Block {
   public index: number;
   public hash: string;
   public prevHash: string;
   public data: string;
   public timestamp: number;
+
   constructor(index, hash, prevHash, data, timestamp) {
     this.index = index;
     this.hash = hash;
@@ -11,6 +14,15 @@ class Block {
     this.data = data;
     this.timestamp = timestamp;
   }
+
+  static calculateBlockHash = (
+    index: number,
+    prevHash: string,
+    timestamp: number,
+    data: string
+  ): string => {
+    return CryptoJS.SHA256(index + prevHash + timestamp + data).toString();
+  };
 }
 
 const genesisBlock: Block = new Block(
@@ -21,8 +33,11 @@ const genesisBlock: Block = new Block(
   132421
 );
 
-let blockchain: [Block] = [genesisBlock];
+let blockchain: Block[] = [genesisBlock];
 
-console.log(blockchain);
+const getBlockchain = (): Block[] => blockchain;
 
+const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
+
+const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
 export {};
